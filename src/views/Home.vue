3f876e7d34200bc7d85844ca5d4cf94e909a89bd
Samantha Diaz <1mariesdiaz@gmail.com>
2018-11-14 21:11:55 -0700

    the first set up

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>This is the homepage?</h1>
+		<h2>{{text}}</h2>	
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text: "This is a sentence?"
+			};
+		}
+	}
+</script>
